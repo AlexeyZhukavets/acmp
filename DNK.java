b5029a50f0de01a06cf47656012c27cdbfeea8c5
AlexeyZhukavets <36003484+AlexeyZhukavets@users.noreply.github.com>
2018-02-05 15:21:27 +0300

    Add files via upload

--- DNK.java
+++ DNK.java
@@ -5,8 +5,18 @@ import java.io.*;
 /**
  * Created by alexe_000 on 25.01.2018.
  */
-public class DNK {
 
+/*
+*_____________________
+*  Progress:
+* _{1}_. Read the file
+* _{2}_. Split the result into two lines, and transform each lines into a char[]
+* _{3}_. Checking for correctness of input data
+* _{4}_. Test the occurrence of a subSequence in a sequence, and recording the result of the test
+*_____________________
+* */
+
+public class DNK {
 
     //Path to the default input-output files
     private static String inputfileName = "d://input.txt";
@@ -15,54 +25,44 @@ public class DNK {
     private static String rezult;
 
     public static void main(String[] args) throws FileNotFoundException {
+        //Subtract an array of strings from a file                     _{1}_
         String[] commonStrings = read().split(" ");
-        commonStrings[0] = commonStrings[0].trim();
-        commonStrings[1] = commonStrings[1].trim();
-        System.out.println(commonStrings[0] + " " + commonStrings[1]);
+
+        //Convert sequences to char array                              _{2}_
         char[] subSequence = commonStrings[0].toCharArray();
         char[] sequence = commonStrings[1].toCharArray();
+
+        //Check sequences                                              _{3}_
         validation(subSequence, sequence);
+
+        //Check whether the subSequence can be evalted into a sequence _{4}_
         consistencyCheck(subSequence, sequence);
     }
 
+    //Check the input sequences
     static void validation(char[] subSecuence, char[] secuence){
+        if(!validAGCT(subSecuence) || !validAGCT(secuence)){
+            System.out.println("Wrong parameters");
+            System.exit(0);
+        }
+    }
 
+    //looking for a discrepancy AGCT in the sequence
+    static boolean validAGCT(char[] secuence){
+        //result of checking
         boolean validSec = false;
-        boolean validSubSec = false;
-        int maxLength = secuence.length + subSecuence.length;
-
-        if(maxLength > 131072){
-            System.out.println("Sorry length is very big");
-            System.exit(1);
-        }
-
-        for (int i = 0; i < subSecuence.length; i++){
-            char value = subSecuence[i];
-            if(value == 'A' || value == 'G' || value == 'C' || value == 'T'){
-                validSubSec = true;
-            }
-            else {
-                validSubSec = false;
-                break;
-            }
-        }
-
         for (int i = 0; i < secuence.length; i++){
             char value = secuence[i];
             if(value == 'A' || value == 'G' || value == 'C' || value == 'T'){
                 validSec = true;
             }else {
-                validSec = false;
-                break;
+                return false;
             }
         }
-
-        if(!validSec || !validSubSec){
-            System.out.println("Wrong parameters");
-            System.exit(1);
-        }
+        return validSec;
     }
 
+    //Check whether the subSequence can be evalted into a sequence
     static void consistencyCheck(char[] subSecuence, char[] secuence){
         int boundCondSub = subSecuence.length;
         int bound = secuence.length;
@@ -81,11 +81,20 @@ public class DNK {
         write("NO");
     }
 
-
-
+    //Read the file if it is less than 256 kb
     public static String read() throws FileNotFoundException {
+        //The result of reading
         StringBuilder sb = new StringBuilder();
+        //Create file
         File file = new File(inputfileName);
+
+        //Checking the file size
+        if(file.length() > 262144){            //262144 B - 256 KB
+            System.out.println("The file size exceeds 256 KB");
+            System.exit(0);
+        }
+
+        //Read file
         try {
             BufferedReader in = new BufferedReader(new FileReader( file.getAbsoluteFile()));
             try {
@@ -103,6 +112,7 @@ public class DNK {
         return sb.toString().trim();
     }
 
+    //Write rezult
     public static void write(String result) {
         File file = new File(outputfilename);
         try {
